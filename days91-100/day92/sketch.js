cbc34910beba34b844c713bc75e59cc829ef4ff7
evapphilips <ep1521@nyu.edu>
2019-08-31 21:41:57 -0500

    adding 92

--- days91-100/day92/sketch.js
+++ days91-100/day92/sketch.js
@@ -1,17 +1,16 @@
 // Eva Philips
-// August 29, 2019
-// 100 Days of Generative Patterns, Day 90
+// August 31, 2019
+// 100 Days of Generative Patterns, Day 92
 // References:
     // This code is largely based on Dan Shiffman's Perlin Noise Flow Field: https://www.youtube.com/watch?v=BjoM9oKOAKY
 
 
 // variables
-let inc = 0.01;
-let zoff = 0;
+let inc1 = 0.01;
+let zoff1 = 0;
+let inc2 = 0.01;
+let zoff2 = 0;
 let r;
-
-
-
 
 
 function setup() {
@@ -23,27 +22,35 @@ function setup() {
 
 function draw() {
 	background(0);
-	
 
-	push();
-	translate(width/2, height/2);
-	inc = map(mouseY, 0, height, 0, .1);
-	zoff = map(mouseX, 0, width, 0, 1);
-	let yoff = 0;
-	let xoff = 0;
-	let stp = map(mouseY, 0, height, .5, 50);
-		for(let i=0; i<width; i+= stp){
-			let r = noise(xoff, yoff, zoff)*width;
-			xoff += inc;
+	zoff1 = map(mouseX, 0, width, 0, 1);
+	let yoff1 = 0;
+	let xoff1 = 0;
+	zoff2 = map(mouseY, 0, width, 0, 1);
+	let yoff2 = 0;
+	let xoff2 = 0;
+	//let stp = map(mouseY, 0, height, .5, 50);
+		for(let i=0; i<width; i+= 5){
+			let r1 = noise(xoff1, yoff1, zoff1)*width;
+			let r2 = noise(xoff2, yoff2, zoff2)*width;
+			xoff1 += inc1;
+			xoff2 += inc2;
+			push();
+			translate(r1, r2);
+			
+			
 	
-			strokeWeight(1);
+			strokeWeight(.25);
 			stroke(255);
 			noFill();
-			ellipse(0, 0, i, r);
+			ellipse(0, 0, r2, r1);
+			pop();
 		}
-		yoff += inc;
+		yoff1 += inc1;
+		yoff2 += inc2;
+
 	
-	pop();
+
 
 }
 
